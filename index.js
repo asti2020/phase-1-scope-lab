07b62c68d40c5695d3f4acab4bc900f8c2802947
Astika Luintel <astild2020@gmail.com>
2022-09-02 22:17:28 -0400

    commit tree 7ae92e242a2e97055860b76ebf3a53982a66473c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    console.log("bob".toUpperCase())
+}
+
+
+function setBestCustomer(){
+    var bestCustomer ;
+}
